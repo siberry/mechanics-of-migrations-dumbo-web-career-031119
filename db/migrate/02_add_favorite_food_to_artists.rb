2df2b7dc229246c2c12a89f7858f88fcc8542ed5
Serena Berry <siberry226@gmail.com>
2019-03-21 15:38:48 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,1 +1,5 @@
-class AddFavoriteFoodToArtists < ActiveRecord::Migration
+class AddFavoriteFoodToArtists < ActiveRecord::Migration[4.2]
+  def change
+    add_column :artists, :favorite_food, :string
+  end
+end
